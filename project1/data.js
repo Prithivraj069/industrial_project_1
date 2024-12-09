61dcbb9f1a7d8b74f6dadb8cf9dcdf15d177a970
Prithivraj Saravanan <prithivisaravanan069@gmail.com>
2024-12-09 07:19:48 +0000

    add foursquare

---
+++ project1/data.js
@@ -0,0 +1,2 @@
+const BASE_API_URL = "https://api.foursquare.com/v3";
+const API_KEY = "fsq3MVMen0Aa8fOoHYckSiXxyOz10OGmfnK9l5YNxqOdKiQ=";
--- project1/script.js
+++ project1/script.js
@@ -1,3 +1,4 @@
 
-
-console.log("hello world")
+document.addEventListener('DOMContentLoaded', function() {
+    const map = createMap('map', 1.3521, 103.8198);
+})
